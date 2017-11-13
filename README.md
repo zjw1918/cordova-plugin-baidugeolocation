8b3869d4f21a9b762cebfa958ff4f37af9bd62f6
Zhou <zjw1918@sina.cn>
2017-11-13 17:05:28 +0800

    1.0.1
    init npm publish

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 ### Android端调用百度Android 定位sdk
 
+### 安装
+
+cordova plugin add cordova-plugin-baidumaplocation --variable ANDROID_KEY=YOUR_BAIDU_ANDROID_APIKEY
+
 ### 用法
 ```js
+// watch
 BaiduGeolocation.startWatch(function(p) {
     console.log(p);
 }, function (err) {
@@ -13,11 +18,24 @@ BaiduGeolocation.startWatch(function(p) {
 setTimeout(function() {
     BaiduGeolocation.stopWatch();
 }, 20 * 1000);
+
+// getCurrent
+BaiduGeolocation.getCurrentPosition(function (res) {
+    console.log(res);
+}, function (err) {
+    console.log(err);
+});
+
 ```
 
+### 说明
+1. ios定位可以直接用cordova-plugin-geolocation, 没有必要再造轮子。返回wgs的经纬度。
+2. 这个插件调用的百度定位sdk。返回bd09ll的经纬度。
 
 ### 参考：
 
 https://github.com/aruis/cordova-plugin-baidumaplocation
 
 https://github.com/ionic-team/cordova-plugin-template
+
+https://github.com/apache/cordova-plugin-geolocation
--- src/android/com/zjw/BaiduGeolocation.java
+++ src/android/com/zjw/BaiduGeolocation.java
@@ -230,7 +230,7 @@ public class BaiduGeolocation extends CordovaPlugin {
     private LocationClientOption getDefaultLocationClientOption() {
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-//        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
         option.setScanSpan(0);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true); // 可选，默认false,设置是否使用gps
@@ -248,7 +248,7 @@ public class BaiduGeolocation extends CordovaPlugin {
     private LocationClientOption getWatchLocationClientOption() {
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-//        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
         option.setScanSpan(1000);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true); // 可选，默认false,设置是否使用gps
